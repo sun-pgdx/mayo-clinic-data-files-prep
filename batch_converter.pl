277efb0b6636d62edd4887f274481a588e1d1e8d
Jaideep Sundaram <jsundaram@pgdx.com>
2018-07-26 20:50:00 -0400

    Refactoring and support for specifying target directory of results to be the sequence directory.

--- batch_converter.pl
+++ batch_converter.pl
@@ -4,6 +4,11 @@ use File::Slurp;
 use File::Basename;
 use File::Path;
 
+use constant TRUE => 1;
+use constant FALSE => 0;
+
+my $write_to_seq_dir = TRUE;
+
 my $file_type_list = ['All', 'Plasma', 'MAF'];
 
 my $outdir = '/tmp/' . File::Basename::basename($0) . '/' . time();
@@ -17,101 +22,139 @@ if (!defined($infile)){
 
 my $seq_id_to_dir_lookup = {};
 
-my @lines = read_file($infile);
-
-chomp @lines;
-
-for my $seq_dir (@lines){
-
-    if ($seq_dir =~ /^\s*$/){
-	next;
-    }
-
-    if ($seq_dir =~ /^#/){
-	next;
-    }
-
-    if (!-e $seq_dir){
-	warn "$seq_dir does not exist";
-    }
-
-    if (!-d $seq_dir){
-	warn "$seq_dir is not a directory";
-    }
-
-    my $seq_id = File::Basename::basename($seq_dir);
-    
-    if ($seq_id =~ m/^(MAYO\d{4}P_PS_Seq2)_\d{8}_\d{6}$/){
-	$seq_id = $1;
-	print "Found seq_id '$seq_id'\n";
-	$seq_id_to_dir_lookup->{$seq_id} = $seq_dir;
-    }
-    else {
-	warn "Could not parse '$seq_id'\n";
-    }
-}
-
+&derive_seq_id_list();
 
 my $seq_id_to_assets_lookup = {};
 
-for my $seq_id (sort keys %{$seq_id_to_dir_lookup}){
-
-    my $lookup = {};
-    
-    my $seq_dir = $seq_id_to_dir_lookup->{$seq_id};
-
-    my $seq_outdir = $outdir . '/' . $seq_id;
-
-    mkpath($seq_outdir) || die "Could not create output directory '$seq_outdir' : $!";
-
-    print "Created seq output directory '$seq_outdir'\n";
-
-    ## MAF file
-    my $maf_file = &get_maf_file($seq_dir, $seq_id);#= $seq_dir . '/' . $seq_id . '*novo/misc/' . $seq_id . '*VV*';
-
-    if (!-e $maf_file){
-	warn "Could not find MAF file '$maf_file'";
-    }
-    else {
-	$seq_id_to_assets_lookup->{$seq_id}->{'MAF'} = $maf_file;
-    }
-    
-    ## Plasma Changes file
-    my $plasma_changes_file = $seq_dir . '/' . File::Basename::basename($seq_dir) . '.PlasmaChanges.txt';
-    if (!-e $plasma_changes_file){
-	die "Could not find Plasma Changes file '$plasma_changes_file'";
-    }
-
-
-    $seq_id_to_assets_lookup->{$seq_id}->{'Plasma'} = $plasma_changes_file;
-
-    
-    ## All Changes file
-    ## E.g.: /cifs/pods/data39-pgdx-pod22/Samples/MAYO0123P_PS_Seq2_20180722_171158/MAYO0123P_PS_Seq2_novo/MAYO0123P_PS_Seq2_novo.allchanges.txt
-    my $all_changes_file = &get_all_changes_file($seq_dir, $seq_id); #$seq_dir . '/' . $seq_id . '*/' . $seq_id . '*.allchanges.txt';
-    if (!-e $all_changes_file){
-	die "Could not find All Changes file '$all_changes_file'";
-    }  
-
-    $seq_id_to_assets_lookup->{$seq_id}->{'All'} = $all_changes_file;
-}
-
+&identify_all_assets();
 
 &write_batch_file();
 
+print File::Spec->rel2abs($0) . " execution completed\n";
+exit(0);
+
+##--------------------------------------------------
+##
+##  END OF MAIN -- SUBROUTINES FOLLOW
+##
+##--------------------------------------------------
+
+sub derive_seq_id_list {
+    
+    my @lines = read_file($infile);
+    
+    chomp @lines;
+    
+    for my $seq_dir (@lines){
+	
+	if ($seq_dir =~ /^\s*$/){
+	    next;
+	}
+	
+	if ($seq_dir =~ /^#/){
+	    next;
+	}
+	
+	if (!-e $seq_dir){
+	    warn "$seq_dir does not exist";
+	}
+	
+	if (!-d $seq_dir){
+	    warn "$seq_dir is not a directory";
+	}
+	
+	my $seq_id = File::Basename::basename($seq_dir);
+	
+	if ($seq_id =~ m/^(MAYO\d{4}P_PS_Seq2)_\d{8}_\d{6}$/){
+	    $seq_id = $1;
+	    print "Found seq_id '$seq_id'\n";
+	    $seq_id_to_dir_lookup->{$seq_id} = $seq_dir;
+	}
+	else {
+	    warn "Could not parse '$seq_id'\n";
+	}
+    }
+}
+
+sub identify_all_assets {
+    
+    my $seq_id_ctr = 0;
+
+    for my $seq_id (sort keys %{$seq_id_to_dir_lookup}){
+
+	$seq_id_ctr++;
+	
+	my $seq_dir = $seq_id_to_dir_lookup->{$seq_id};
+
+	my $seq_outdir = $outdir . '/' . $seq_id;
+
+	mkpath($seq_outdir) || die "Could not create output directory '$seq_outdir' : $!";
+
+	print "Created seq output directory '$seq_outdir'\n";
+
+	## MAF file
+	my $maf_file = &get_maf_file($seq_dir, $seq_id);#= $seq_dir . '/' . $seq_id . '*novo/misc/' . $seq_id . '*VV*';
+
+	if (!-e $maf_file){
+	    warn "Could not find MAF file '$maf_file'";
+	}
+	else {
+	    $seq_id_to_assets_lookup->{$seq_id}->{'MAF'} = $maf_file;
+	}
+	
+	## Plasma Changes file
+	my $plasma_changes_file = $seq_dir . '/' . File::Basename::basename($seq_dir) . '.PlasmaChanges.txt';
+	if (!-e $plasma_changes_file){
+	    die "Could not find Plasma Changes file '$plasma_changes_file'";
+	}
+
+
+	$seq_id_to_assets_lookup->{$seq_id}->{'Plasma'} = $plasma_changes_file;
+
+	
+	## All Changes file
+	## E.g.: /cifs/pods/data39-pgdx-pod22/Samples/MAYO0123P_PS_Seq2_20180722_171158/MAYO0123P_PS_Seq2_novo/MAYO0123P_PS_Seq2_novo.allchanges.txt
+	my $all_changes_file = &get_all_changes_file($seq_dir, $seq_id); #$seq_dir . '/' . $seq_id . '*/' . $seq_id . '*.allchanges.txt';
+	if (!-e $all_changes_file){
+	    die "Could not find All Changes file '$all_changes_file'";
+	}  
+
+	$seq_id_to_assets_lookup->{$seq_id}->{'All'} = $all_changes_file;
+
+	if ($seq_id_ctr == 1){
+	    last;
+	}
+    }
+}
+
+
 sub write_batch_file {
     
-    open (OUTFILE, ">$outfile") || die "Could not open file '$outfile' in write modee :$!";
+    open (OUTFILE, ">$outfile") || die "Could not open file '$outfile' in write mode :$!";
     
     for my $seq_id (sort keys %{$seq_id_to_assets_lookup}){
 
-	print OUTFILE "\n\n$seq_id\n";
+	print OUTFILE "\n\necho 'Processing $seq_id'\n";
 	
 	for my $file_type (sort @{$file_type_list}){
 	    
 	    my $file = $seq_id_to_assets_lookup->{$seq_id}->{$file_type};
+
+	    my $results_dir = $outdir . '/' . $seq_id;
 	    
-	    my $cmd = "python converter.py $file $seq_id --outdir $outdir/$seq_id";
+	    if ($write_to_seq_dir){
+
+		my $seq_dir = $seq_id_to_dir_lookup->{$seq_id};
+
+		$results_dir = $seq_dir . '/vcf-data-prep/';
+	    }
+	    
+	    if (!-e $results_dir){
+		print OUTFILE "mkdir -p $results_dir\n";
+		#mkpath($results_dir) || die "Could not create results directory '$results_dir' : $!";
+	    }
+		    
+	    my $cmd = "python converter.py $file $seq_id --outdir $results_dir";
 	    
 	    print OUTFILE $cmd . "\n";
 	}
